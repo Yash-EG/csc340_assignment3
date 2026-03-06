eb3e51d64d2cde92e11e1d33dcb850d04380aa94
Yash-EG <yashpatel.eg@gmail.com>
2026-03-06 08:48:59 -0500

    springboot

---
+++ src/test/java/com/yash/antiheros/AntiherosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.antiheros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiherosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
